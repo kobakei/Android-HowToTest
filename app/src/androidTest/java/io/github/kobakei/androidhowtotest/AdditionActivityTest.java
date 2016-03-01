bcc4166cd64144b80bd65c2a8324c3d29439a910
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2016-03-02 02:46:43 +0900

    Add espresso test

---
+++ app/src/androidTest/java/io/github/kobakei/androidhowtotest/AdditionActivityTest.java
@@ -0,0 +1,36 @@
+package io.github.kobakei.androidhowtotest;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * UI test with JUnit4 & Espresso.
+ * Created by keisuke on 16/03/02.
+ */
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class AdditionActivityTest {
+
+    @Rule
+    public ActivityTestRule<AdditionActivity> activityTestRule = new ActivityTestRule<>(AdditionActivity.class);
+
+    @Test
+    public void onAddClicked_isCorrect() {
+        onView(withId(R.id.editText1)).perform(typeText("123"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.editText2)).perform(typeText("456"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("579")));
+    }
+
+}
